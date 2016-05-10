ecaa296df59ee3d68b5e468b335fd619697e1520
Julian Popescu <jpopesculian@gmail.com>
2016-05-10 14:10:59 -0400

    deleted data dir

--- sysmonitor/server.py
+++ sysmonitor/server.py
@@ -1,7 +1,7 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def get_stats():
-    return 'Hello World'
+@app.route('/<resource>/now', methods=['GET'])
+def get_stats(resource):
+    return
 
