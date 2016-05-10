771d629145c87b305f0e7469ee2cb8ecd7ca2ee5
Julian Popescu <jpopesculian@gmail.com>
2016-05-10 15:05:12 -0400

    basic server

--- sysmonitor/repo.py
+++ sysmonitor/repo.py
@@ -30,3 +30,38 @@ def insert(conn, table, data):
     c = conn.cursor()
     c.execute("INSERT INTO %s ('data') VALUES ('%s')" % (table, json.dumps(data)))
     conn.commit()
+
+def fetch(conn, table, sort=1, page=0, size=20):
+    c = conn.cursor()
+    sort = "DESC" if sort is 1 else "ASC"
+    offset = page*size
+    c.execute('''
+        SELECT * FROM %s
+        ORDER BY timestamp %s
+        LIMIT %s
+        OFFSET %s
+    ''' % (table, sort, size, offset))
+    result = [_parse_row(row) for row in c.fetchall()]
+    return {
+        'page': page,
+        'length': len(result),
+        'events': result
+    }
+
+
+
+
+def fetch_last(conn, table):
+    c = conn.cursor()
+    c.execute('''
+        SELECT * FROM %s ORDER BY timestamp DESC LIMIT 1
+    ''' % table)
+    result = c.fetchone()
+    return _parse_row(result)
+
+def _parse_row(row):
+    return {
+        'id': row[0],
+        'timestamp': row[1],
+        'stats': json.loads(row[2])
+    }
--- sysmonitor/server.py
+++ sysmonitor/server.py
@@ -1,7 +1,18 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, jsonify
+from .repo import fetch, fetch_last, connect
 
-@app.route('/<resource>/now', methods=['GET'])
+app = Flask(__name__)
+app.debug = True
+
+@app.route('api/v1/<resource>', methods=['GET'])
+def get_stat_list(resource):
+    conn = connect()
+    result = fetch(conn, str.capitalize(resource))
+    return jsonify(**result)
+
+@app.route('api/v1/<resource>/last', methods=['GET'])
 def get_stats(resource):
-    return
+    conn = connect()
+    result = fetch_last(conn, str.capitalize(resource))
+    return jsonify(**result)
 
