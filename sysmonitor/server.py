c071a116693b78b74105725dc34314ed3af06d37
Julian Popescu <jpopesculian@gmail.com>
2016-05-10 15:19:35 -0400

    added query params to list route

--- sysmonitor/server.py
+++ sysmonitor/server.py
@@ -1,16 +1,23 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from .repo import fetch, fetch_last, connect
 
 app = Flask(__name__)
-app.debug = True
+# app.debug = True
 
-@app.route('api/v1/<resource>', methods=['GET'])
+@app.route('/api/v1/<resource>', methods=['GET'])
 def get_stat_list(resource):
     conn = connect()
-    result = fetch(conn, str.capitalize(resource))
+    filters = dict()
+    for arg_name in ['sort', 'page', 'size']:
+        try:
+            arg = int(request.args.get(arg_name))
+        except (ValueError, TypeError):
+            continue
+        filters[arg_name] = arg
+    result = fetch(conn, str.capitalize(resource), **filters)
     return jsonify(**result)
 
-@app.route('api/v1/<resource>/last', methods=['GET'])
+@app.route('/api/v1/<resource>/last', methods=['GET'])
 def get_stats(resource):
     conn = connect()
     result = fetch_last(conn, str.capitalize(resource))
